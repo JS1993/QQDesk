813f70867b64bbfb511ae33a3868db45fc06d469
susu <28025920@qq.com>
2016-04-12 10:13:39 +0800

    仿QQ聊天界面

---
+++ QQDesk/QQDesk/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  QQDesk
+//
+//  Created by  江苏 on 16/4/12.
+//  Copyright © 2016年 jiangsu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
